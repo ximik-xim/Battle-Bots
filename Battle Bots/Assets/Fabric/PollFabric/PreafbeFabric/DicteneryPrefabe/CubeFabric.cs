f52f904327a771bfb71347af34057c8e918ae354
ximik-xim <45805384+ximik-xim@users.noreply.github.com>
2022-11-12 21:02:24 +0300

    Изменение фабрики

--- Battle Bots/Assets/Fabric/PollFabric/PreafbeFabric/DicteneryPrefabe/CubeFabric.cs
+++ Battle Bots/Assets/Fabric/PollFabric/PreafbeFabric/DicteneryPrefabe/CubeFabric.cs
@@ -1,19 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class CubeFabric : EntityFabric<CubeEntity,CubeEntity.TypeCube>
+public class CubeFabric : EntityFabric<CubeEntity.TypeCube, CubeEntity>
 {
    [SerializeField]
     private SerializeDictionary<CubeHaracteristic.TypeRole, CubeHaracteristic> _haracteristics;
-    protected override void StartObject()
+
+    private void Awake()
     {
+        serializeDictionary.Initialization();
         _haracteristics.Initialization();
     }
 
     public override void Spawn(CubeEntity.TypeCube type, int count, Vector3 position)
     {
+        int countEntity = 0;
+        for (int i = 0; i <_pool.DisavleObject.Count ; i++)
+        {
+            if (_pool.DisavleObject[i].CubeType == type)
+            {
+                _pool.DisavleObject[i].SetHar(RandomTypeEnum());
+                _pool.DisavleObject[i].gameObject.SetActive(true);
+                _pool.DisavleObject[i].gameObject.transform.position = position;
+
+                countEntity++;
+                if (countEntity == count)
+                {
+                    return;
+                }
+            }
+        }
         
+        for (int i = countEntity; i < count; i++)
+        {
+           var entity = InstantiatePrefabe(serializeDictionary[type]);
+           entity.gameObject.transform.position = position;
+        }
+        
+    }
+    
+    protected CubeHaracteristic RandomTypeEnum()
+    {
+        int countElementType = Enum.GetNames(typeof(CubeHaracteristic.TypeRole)).Length;
+        int idType = Random.Range(1, countElementType); 
+        CubeHaracteristic.TypeRole type = (CubeHaracteristic.TypeRole)Enum.GetValues(typeof(CubeHaracteristic.TypeRole)).GetValue(idType);
+        CubeHaracteristic prefabEntity = _haracteristics[type];
+        
+        return prefabEntity;
     }
 
     public void Spawn(CubeEntity.TypeCube type, CubeHaracteristic.TypeRole role, int count, Vector3 position)
@@ -23,6 +59,6 @@ public class CubeFabric : EntityFabric<CubeEntity,CubeEntity.TypeCube>
 
     protected override void AddParametersInstantiatePrefabe(CubeEntity cubeEntity)
     {
-
+        cubeEntity.SetHar(RandomTypeEnum());
     }
 }
