21711e11a753d7faf35e7238069223eb5262962b
ximik-xim <45805384+ximik-xim@users.noreply.github.com>
2022-11-12 21:05:16 +0300

    Переделка префабов

--- Battle Bots/Assets/PoolReact/Entity/CubeEntity/CubeEntity.cs
+++ Battle Bots/Assets/PoolReact/Entity/CubeEntity/CubeEntity.cs
@@ -11,18 +11,58 @@ public class CubeEntity : Entity
         Red,
         Green
     }
-    
-    public TypeCube Type
+
+    public float MoveSpeed
+    {
+        get => _moveSpeed;
+    }
+    public float Damage
+    {
+        get => _damage;
+    }
+    public float RangeAttake
+    {
+        get => _rangeAttack;
+    }
+    public float DetectionRange
+    {
+        get => _detectionRange;
+    }
+
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _rangeAttack;
+    [SerializeField] private float _detectionRange;
+
+    public TypeCube CubeType
     {
         get => _typeCube;
     }
 
     [SerializeField]
     private TypeCube _typeCube;
-    public CubeHaracteristic CubeHaracteristic { get; private set; }
-
+    
     public void SetHar(CubeHaracteristic haracteristic)
     {
-        CubeHaracteristic = haracteristic;
+        SetHara(haracteristic);
+
+        _moveSpeed = haracteristic.MoveSpeed;
+        _damage = haracteristic.Damage;
+        _rangeAttack = haracteristic.RangeAttake;
+        _detectionRange = haracteristic.DetectionRange;
+    }
+
+    public override bool Equals(object other)
+    {
+        if (other.GetType()==this.GetType())
+        {
+            CubeEntity entity = (CubeEntity) other;
+            if (entity.CubeType == this.CubeType)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Battle Bots/Assets/PoolReact/Entity/Entity.cs
+++ Battle Bots/Assets/PoolReact/Entity/Entity.cs
@@ -1,18 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public abstract class Entity : PoolReact
 {
+    [SerializeField]
+    private UnityEvent<float> _updateHp;
+    public float Hp
+    {
+        get => _Hp;
+        private set
+        {
+            _Hp = value;
+            
+            if (_Hp <= 0)
+            {
+                _Hp = 0;
+                gameObject.SetActive(false);
+            }
+        }
+        
+    }
+    
+    [SerializeField] private float _Hp;
+    
+    public void SetHara(Haracteristic haracteristic)
+    {
+        _Hp = haracteristic.Hp;
+    }
 
+    public void TakeDamage(float damage)
+    {
+        Hp -= damage;
+    }
     
-    
-    
-    
-    
-
-    
+    public void Kill()
+    {
+        Hp = 0;
+    }
     
 }
